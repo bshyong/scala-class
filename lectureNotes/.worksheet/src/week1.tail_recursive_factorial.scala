3ec8ca1d003ddccbc61e12cc14dcf26c2d19a2e3
Ben Shyong <likeachild@gmail.com>
2013-09-18 00:07:05 -0400

    tail recursive factorial

---
+++ lectureNotes/.worksheet/src/week1.tail_recursive_factorial.scala
@@ -0,0 +1,12 @@
+package week1
+
+object tail_recursive_factorial {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(180); 
+	def factorial(n: Int): Int = {
+		def loop(acc: Int, n: Int): Int = {
+			if (n==0) acc
+			else loop(acc*n, n-1)
+		}
+		loop(1, n)
+	};System.out.println("""factorial: (n: Int)Int""");$skip(14); val res$0 = 
+	factorial(4);System.out.println("""res0: Int = """ + $show(res$0))}
+}
