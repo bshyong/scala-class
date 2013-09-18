e6e348f292d3f153be394e90a759654ad5a140f9
Ben Shyong <likeachild@gmail.com>
2013-09-17 23:27:20 -0400

    refactor sqrt function to take advantage of scoping

--- lectureNotes/.worksheet/src/week1.sqrt1.scala
+++ lectureNotes/.worksheet/src/week1.sqrt1.scala
@@ -1,19 +1,21 @@
 package week1
 
 object sqrt1 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(70); 
-	def abs(x: Double) = if (x<0) -x else x;System.out.println("""abs: (x: Double)Double""");$skip(126); 
+	def abs(x: Double) = if (x<0) -x else x;System.out.println("""abs: (x: Double)Double""");$skip(342); 
 	
-	def sqrtIter(guess: Double, x: Double): Double =
-		if (isGoodEnough(guess, x)) guess
-		else sqrtIter(improve(guess, x), x);System.out.println("""sqrtIter: (guess: Double, x: Double)Double""");$skip(133); 
-                                                	
-	def isGoodEnough(guess: Double, x: Double) =
-		abs(guess * guess - x) / x < 0.001;System.out.println("""isGoodEnough: (guess: Double, x: Double)Boolean""");$skip(69); 
+	def sqrt(x: Double) = {
+		def sqrtIter(guess: Double): Double =
+			if (isGoodEnough(guess)) guess
+			else sqrtIter(improve(guess))
+	                                                	
+		def isGoodEnough(guess: Double) =
+			abs(guess * guess - x) / x < 0.001
+		
+		def improve(guess: Double) =
+			(guess + x / guess) / 2
 	
-	def improve(guess: Double, x: Double) =
-		(guess + x / guess) / 2;System.out.println("""improve: (guess: Double, x: Double)Double""");$skip(41); 
-
-	def sqrt(x: Double) = sqrtIter(1.0, x);System.out.println("""sqrt: (x: Double)Double""");$skip(11); val res$0 = 
+		 sqrtIter(1.0)
+	};System.out.println("""sqrt: (x: Double)Double""");$skip(11); val res$0 = 
 	
 	sqrt(2);System.out.println("""res0: Double = """ + $show(res$0));$skip(9); val res$1 = 
 	sqrt(4);System.out.println("""res1: Double = """ + $show(res$1));$skip(12); val res$2 = 
