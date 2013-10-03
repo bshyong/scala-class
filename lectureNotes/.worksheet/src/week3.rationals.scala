d76724b1bf9a3791a98b004c5e71e1640a7d3a85
Ben Shyong <ben.shyong@gmail.com>
2013-10-03 01:43:25 -0400

    operators

--- lectureNotes/.worksheet/src/week3.rationals.scala
+++ lectureNotes/.worksheet/src/week3.rationals.scala
@@ -5,9 +5,9 @@ object rationals {;import org.scalaide.worksheet.runtime.library.WorksheetSuppor
   val y = new Rational(2,2);System.out.println("""y  : week3.Rational = """ + $show(y ));$skip(28); 
   val z = new Rational(3,2);System.out.println("""z  : week3.Rational = """ + $show(z ));$skip(10); val res$0 = 
   x.numer;System.out.println("""res0: Int = """ + $show(res$0));$skip(10); val res$1 = 
-  x.denom;System.out.println("""res1: Int = """ + $show(res$1));$skip(18); val res$2 = 
-  x.sub(y).sub(z);System.out.println("""res2: week3.Rational = """ + $show(res$2));$skip(11); val res$3 = 
-  x.max(z);System.out.println("""res3: week3.Rational = """ + $show(res$3));$skip(18); val res$4 = 
+  x.denom;System.out.println("""res1: Int = """ + $show(res$1));$skip(12); val res$2 = 
+  x - y - z;System.out.println("""res2: week3.Rational = """ + $show(res$2));$skip(10); val res$3 = 
+  x max z;System.out.println("""res3: week3.Rational = """ + $show(res$3));$skip(18); val res$4 = 
   new Rational(2);System.out.println("""res4: week3.Rational = """ + $show(res$4))}
 }
 
@@ -22,18 +22,18 @@ class Rational(x: Int, y: Int){
 	def numer = x / g
 	def denom = y / g
 	
-	def add(that: Rational) =
+	def + (that: Rational) =
 		new Rational(
 			numer * that.denom + that.numer * denom,
 			denom * that.denom)
 			
-	def neg: Rational = new Rational(-numer, denom)
+	def unary_- : Rational = new Rational(-numer, denom)
 	
-	def sub(that: Rational) = add(that.neg)
+	def - (that: Rational) = this + -that
 	
-	def less(that: Rational) = this.numer * that.denom < that.numer * this.denom
+	def < (that: Rational) = this.numer * that.denom < that.numer * this.denom
 	
-	def max(that: Rational) = if (this.less(that)) that else this
+	def max(that: Rational) = if (this < that) that else this
 	
 	override def toString = numer + "/" + denom
 }
--- lectureNotes/src/week3/rationals.sc
+++ lectureNotes/src/week3/rationals.sc
@@ -6,8 +6,8 @@ object rationals {
   val z = new Rational(3,2)                       //> z  : week3.Rational = 3/2
   x.numer                                         //> res0: Int = 1
   x.denom                                         //> res1: Int = 2
-  x.sub(y).sub(z)                                 //> res2: week3.Rational = -2/1
-  x.max(z)                                        //> res3: week3.Rational = 3/2
+  x - y - z                                       //> res2: week3.Rational = -2/1
+  x max z                                         //> res3: week3.Rational = 3/2
   new Rational(2)                                 //> res4: week3.Rational = 2/1
 }
 
@@ -22,18 +22,18 @@ class Rational(x: Int, y: Int){
 	def numer = x / g
 	def denom = y / g
 	
-	def add(that: Rational) =
+	def + (that: Rational) =
 		new Rational(
 			numer * that.denom + that.numer * denom,
 			denom * that.denom)
 			
-	def neg: Rational = new Rational(-numer, denom)
+	def unary_- : Rational = new Rational(-numer, denom)
 	
-	def sub(that: Rational) = add(that.neg)
+	def - (that: Rational) = this + -that
 	
-	def less(that: Rational) = this.numer * that.denom < that.numer * this.denom
+	def < (that: Rational) = this.numer * that.denom < that.numer * this.denom
 	
-	def max(that: Rational) = if (this.less(that)) that else this
+	def max(that: Rational) = if (this < that) that else this
 	
 	override def toString = numer + "/" + denom
 }
